16614419ac8f7be01cc8b808d2e6218c1676e03a
ctcommon <ctcommon@163.com>
2017-11-24 23:32:52 +0800

    利用epoll同时处理TCP请求和UDP请求的回射服务器

---
+++ TCP_UDP_test.c
@@ -0,0 +1,147 @@
+/*有不少服务器程序都能同时监听多个端口，比如超级服务inetd,从bind系统调用的
+ * 角度来说，一个socket只能与一个socket地址绑定，即一个socket只能用来监听
+ * 一个端口。因此服务器如果要同时监听多个端口，就必须创建多个socket，并将
+ * 它们分别绑定到各个端口上。I/O复用技术就有了用武之地。另外，即使是同一个
+ * 端口，如果服务器要同时处理该端口上的TCP和UDP请求，则也需要创建两个不同的
+ * socket：一个是流socket，另一个是数据报socket，并将它们都绑定到该端口上*/
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#define MAX_EVENT_NUMBER 1024
+#define TCP_BUFFER_SIZE 512
+#define UDP_BUFFER_SIZE 1024
+
+int setnonblocking(int fd)
+{
+	int old_option = fcntl(fd,F_GETFL);
+	int new_option = old_option | O_NONBLOCK;
+	fcntl(fd,F_SETFL,new_option);
+	return old_option;
+}
+void addfd(int epollfd, int fd)
+{
+	epoll_event event;
+	event.data.fd = fd;
+	event.events = EPOLLIN | EPOLLET;
+	epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event);
+	setnonblocking(fd);
+}
+int main(int argc, char* argv[])
+{
+	if(argc <= 2)
+	{
+		printf("usage: %s ip_address port_number\n",basename(argv[0]));
+		return 1;
+	}
+	const char* ip = argv[1];
+	int port = atoi(argv[2]);
+
+	sockaddr_in address;
+	bzero(&address,sizeof(address));
+	address.sin_family = AF_INET;
+	inet_pton(AF_INET,ip,&address.sin_addr);
+	address.sin_port = htons(port);
+	
+	/*创建TCP socket，并将其绑定到端口port上*/
+	int listenfd = socket(PF_INET,SOCK_STREAM,0);
+	assert(listenfd >= 0);
+
+	int ret = bind(listenfd,(struct sockaddr*)&address,sizeof(address));
+	assert(ret != -1);
+
+	ret = listen(listenfd,5);
+	assert(ret != -1);
+
+	/*创建UDP socket，并将其绑定到端口port上*/
+	bzero(&address,sizeof(address));
+	address.sin_family = AF_INET;
+	inet_pton(AF_INET,ip,&address.sin_addr);
+	address.sin_port = htons(port);
+
+	int udpfd = socket(PF_INET,SOCK_DGRAM,0);
+	assert(udpfd >= 0);
+
+	ret = bind(udpfd,(struct sockaddr*)&address,sizeof(address));
+	assert(ret != -1);
+
+	epoll_event events[MAX_EVENT_NUMBER];
+	int epollfd = epoll_create(5);
+	assert(epollfd != -1);
+	/*注册TCP socket和UDP socket上的可读事件*/
+	addfd(epollfd,listenfd);
+	addfd(epollfd,udpfd);
+
+	while(1)
+	{
+		int number = epoll_wait(epollfd,events,MAX_EVENT_NUMBER,-1);
+		if(number < 0)
+		{
+			printf("epoll failure\n");
+			break;
+		}
+		for(int i=0; i<number; i++)
+		{
+			int sockfd = events[i].data.fd;
+			if(sockfd == listenfd)
+			{
+				struct sockaddr_in client_address;
+				socklen_t client_addrlength = sizeof(client_address);
+				int connfd = accept(listenfd,(struct sockaddr*)&client_address,
+														&client_addrlength);
+				addfd(epollfd, connfd);
+			}
+			else if(sockfd == udpfd)
+			{
+				char buf[UDP_BUFFER_SIZE];
+				memset(buf,'\0',UDP_BUFFER_SIZE);
+				struct sockaddr_in client_address;
+				socklen_t client_addrlength = sizeof(client_address);
+
+				ret = recvfrom(udpfd,buf,UDP_BUFFER_SIZE-1,0,
+											(struct sockaddr*)&client_address,&client_addrlength);
+				if(ret > 0)
+				{
+					sendto(udpfd,buf,UDP_BUFFER_SIZE-1,0,
+									(struct sockaddr*)&client_address,client_addrlength);
+				}
+			}
+			else if(events[i].events & EPOLLIN)
+			{
+				char buf[TCP_BUFFER_SIZE];
+				while(1)
+				{
+					memset(buf,'\0',TCP_BUFFER_SIZE);
+					ret =recv(sockfd,buf,TCP_BUFFER_SIZE-1,0);
+					if(ret < 0)
+					{
+						if((ret == EAGAIN) || (errno == EWOULDBLOCK))
+							break;
+						close(sockfd);
+						break;
+					}
+					else if(ret == 0)
+					{
+						close(sockfd);
+					}
+					else
+						send(sockfd,buf,ret,0);
+				}
+			}
+			else
+				printf("something else happened\n");
+		}
+	}
+	close(listenfd);
+	return 0;
+}
