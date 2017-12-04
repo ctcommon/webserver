fd0580531c938677a16dc22edee5eb6bd3cc5611
ctcommon <ctcommon@163.com>
2017-12-04 14:37:07 +0800

    在多线程程序中调用fork函数

---
+++ pthread_fork.c
@@ -0,0 +1,55 @@
+/*如果在多线程中的某个线程调用了fork函数，新的进程并不会自动创建和父进程相同
+ * 数量的线程，只会是调用fork的那个线程的完整复制，并且还会自动继承父进程中
+ * 互斥锁(条件变量与之类似)的状态，也就是说在父进程中被加锁的互斥锁在子进程中
+ * 也是加锁的，这会导致在子进程再次对互斥锁进行加锁的时候可能导致死锁*/
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+
+pthread_mutex_t mutex;
+/*子线程运行的函数，它首先获得互斥锁mutex，然后暂停5s，再释放该互斥锁*/
+void* another(void* arg)
+{
+	printf("in child thread, lock the mutex\n");
+	pthread_mutex_lock(&mutex);
+	sleep(5);
+	pthread_mutex_unlock(&mutex);
+}
+
+int main()
+{
+	pthread_mutex_init(&mutex,NULL);
+	pthread_t id;
+	pthread_create(&id, NULL, another, NULL);
+	/*父进程中的主线程暂停1s，以确保在执行fork之前，子线程已经开始运行并获得了
+	 * 互斥变量mutex*/
+	sleep(1);
+	int pid = fork();
+	if(pid < 0)
+	{
+		pthread_join(id,NULL);
+		pthread_mutex_destroy(&mutex);
+		return 1;
+	}
+	else if(pid == 0)
+	{
+		printf("I am in the child, want to get the lock\n");
+		/*子进程从父进程继承了互斥锁mutex的状态，该互斥锁处于锁住的状态，这是由父
+		 * 进程中的子进程执行pthread_mutex_lock引起的，因此，下面这句加锁操作会一
+		 * 直阻塞，尽管从逻辑上来说它是不应该阻塞的*/
+		pthread_mutex_lock(&mutex);
+		printf("I can not to run to here, oop...\n");
+		pthread_mutex_unlock(&mutex);
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+	}
+	pthread_join(id,NULL);
+	pthread_mutex_destroy(&mutex);
+	return 0;
+}
